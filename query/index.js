5d3812f38a3129848c59db323aa344e02f3ddc65
Evangelos Kolimitras <evangeloskolimitras@outlook.com>
2020-08-31 15:15:38 +0200

    query service implemented

--- query/index.js
+++ query/index.js
@@ -5,11 +5,25 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+
+const posts = {};
 app.get("/posts", (req, res) => {
+    res.send(posts);
+});
 
-})
 app.post("/events", (req, res) => {
-
-})
+    const { type, data } = req.body;
+    if (type === "PostCreated") {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    }
+    if (type === "CommentCreated") {
+        const { id, content, postId } = req.body;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+    console.log(posts)
+    res.send({});
+});
 
 app.listen(4002, () => console.log("Listeing on 4002."))
